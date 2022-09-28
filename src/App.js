be307fae85fb02313dfa9ba4a2d57db716cddd34
getusha <getudesta4@gmail.com>
2022-09-28 20:45:05 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Sidebar from "./Components/Basic/Sidebar";
 import Dashboard from "./Components/Pages/Dashboard";
 import HomePage from "./Components/Pages/Homepage";
 import NewAccount from "./Components/Pages/NewAccount";
@@ -12,11 +13,13 @@ function App() {
           <Route path="/">
             <Route path="" element={<HomePage />} />
             <Route path="/register" element={<NewAccount />} />
-            <Route path="dash/" >
-              <Route path="" element={<Dashboard />} />
-              
+            <Route path="app/" >
+              <Route path="dashboard" element={
+                <Dashboard />
+              } />
             </Route>
           </Route>
+        
         </Routes>
 
       </BrowserRouter>
--- src/Components/Basic/Sidebar.jsx
+++ src/Components/Basic/Sidebar.jsx
@@ -1,4 +1,18 @@
+import { ActionIcon, Avatar, Grid, Group, Paper, ScrollArea, Stack, NavLink, MantineProvider, ThemeIcon, SimpleGrid, Card, Text, Menu, Modal, TextInput, Button, Textarea, Radio, MultiSelect, Select } from "@mantine/core";
+import { IconAdjustments, IconBug, IconCalendar, IconDashboard, IconDotsVertical, IconEdit, IconFlag, IconFlag2, IconFlag3, IconInfoCircle, IconMenu, IconMenu2, IconMessage, IconPencil, IconPlus, IconStack, IconStatusChange, IconTrash, IconUser, IconUserCheck, IconUserCircle, IconUsers } from "@tabler/icons";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+
 const Sidebar = (props) => {
+
+    const [bigSide, setBigSide] = useState(true);
+    const toggleSidebar = () => {
+        setBigSide(!bigSide)
+    }
+
+    const shet = useLocation();
+    console.log(shet.pathname)
+
     return (
         <>
             <MantineProvider theme={{ fontFamily: "'Work Sans', 'sans-serif'", fontSizes: "1.05rem" }}>
@@ -7,17 +21,17 @@ const Sidebar = (props) => {
                     <Grid.Col span={bigSide ? 2 : 0.2} sx={{ height: "100%", width: "100%", position: "fixed" }}>
                         <Paper p="md" className="sidemeat" sx={{ height: "100%", zIndex: 3 }} radius={"xs"}>
 
-                            <NavLink className="navlink" active label="Dashboard" icon={
+                            <NavLink active={shet.pathname.includes("dashboard")} className="navlink" label="Dashboard" icon={
                                 <ThemeIcon variant="gradient" gradient={{ from: 'indigo', to: 'cyan' }}>
                                     <IconDashboard />
                                 </ThemeIcon>
                             } sx={{ padding: "15px", fontWeight: "500", fontSize: "1.05rem" }} />
-                            <NavLink className="navlink" label="Projects" icon={
+                            <NavLink active={shet.pathname.includes("projects")} className="navlink" label="Projects" icon={
                                 <ThemeIcon variant="gradient" gradient={{ from: 'indigo', to: 'cyan' }}>
                                     <IconStack />
                                 </ThemeIcon>
                             } sx={{ padding: "15px", fontSize: "1.05rem" }} />
-                            <NavLink className="navlink" label="Isuess" icon={
+                            <NavLink className="navlink" label="Reports" icon={
                                 <ThemeIcon variant="gradient" gradient={{ from: 'indigo', to: 'cyan' }}>
                                     <IconBug />
                                 </ThemeIcon>
